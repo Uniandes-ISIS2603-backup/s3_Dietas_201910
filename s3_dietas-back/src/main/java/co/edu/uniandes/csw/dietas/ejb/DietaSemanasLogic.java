9a981f6537e0bc7ec79ea2a5c5e3a7b3bea41f4c
Juan Antonio Restrepo <juan981106@hotmail.com>
2019-04-11 20:12:57 -0500

    commit de desespero

--- s3_dietas-back/src/main/java/co/edu/uniandes/csw/dietas/ejb/DietaSemanasLogic.java
+++ s3_dietas-back/src/main/java/co/edu/uniandes/csw/dietas/ejb/DietaSemanasLogic.java
@@ -1,105 +1,103 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package co.edu.uniandes.csw.dietas.ejb;
-//
-//import java.util.List;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import javax.ejb.Stateless;
-//import javax.inject.Inject;
-//
-///**
-// *
-// * @author Alejandra Bravoa
-// */
-//@Stateless
-//public class DietaSemanasLogic {
-//    private static final Logger LOGGER = Logger.getLogger(BookAuthorLogic.class.getName());
-//
-//    @Inject
-//    private BookPersistence bookPersistence;
-//
-//    @Inject
-//    private AuthorPersistence authorPersistence;
-//
-//    /**
-//     * Asocia un Author existente a un Book
-//     *
-//     * @param booksId Identificador de la instancia de Book
-//     * @param authorsId Identificador de la instancia de Author
-//     * @return Instancia de AuthorEntity que fue asociada a Book
-//     */
-//    public AuthorEntity addAuthor(Long booksId, Long authorsId) {
-//        LOGGER.log(Level.INFO, "Inicia proceso de asociarle un autor al libro con id = {0}", booksId);
-//        AuthorEntity authorEntity = authorPersistence.find(authorsId);
-//        BookEntity bookEntity = bookPersistence.find(booksId);
-//        bookEntity.getAuthors().add(authorEntity);
-//        LOGGER.log(Level.INFO, "Termina proceso de asociarle un autor al libro con id = {0}", booksId);
-//        return authorPersistence.find(authorsId);
-//    }
-//
-//    /**
-//     * Obtiene una colección de instancias de AuthorEntity asociadas a una
-//     * instancia de Book
-//     *
-//     * @param booksId Identificador de la instancia de Book
-//     * @return Colección de instancias de AuthorEntity asociadas a la instancia
-//     * de Book
-//     */
-//    public List<AuthorEntity> getAuthors(Long booksId) {
-//        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los autores del libro con id = {0}", booksId);
-//        return bookPersistence.find(booksId).getAuthors();
-//    }
-//
-//    /**
-//     * Obtiene una instancia de AuthorEntity asociada a una instancia de Book
-//     *
-//     * @param booksId Identificador de la instancia de Book
-//     * @param authorsId Identificador de la instancia de Author
-//     * @return La entidad del Autor asociada al libro
-//     */
-//    public AuthorEntity getAuthor(Long booksId, Long authorsId) {
-//        LOGGER.log(Level.INFO, "Inicia proceso de consultar un autor del libro con id = {0}", booksId);
-//        List<AuthorEntity> authors = bookPersistence.find(booksId).getAuthors();
-//        AuthorEntity authorEntity = authorPersistence.find(authorsId);
-//        int index = authors.indexOf(authorEntity);
-//        LOGGER.log(Level.INFO, "Termina proceso de consultar un autor del libro con id = {0}", booksId);
-//        if (index >= 0) {
-//            return authors.get(index);
-//        }
-//        return null;
-//    }
-//
-//    /**
-//     * Remplaza las instancias de Author asociadas a una instancia de Book
-//     *
-//     * @param booksId Identificador de la instancia de Book
-//     * @param list Colección de instancias de AuthorEntity a asociar a instancia
-//     * de Book
-//     * @return Nueva colección de AuthorEntity asociada a la instancia de Book
-//     */
-//    public List<AuthorEntity> replaceAuthors(Long booksId, List<AuthorEntity> list) {
-//        LOGGER.log(Level.INFO, "Inicia proceso de reemplazar los autores del libro con id = {0}", booksId);
-//        BookEntity bookEntity = bookPersistence.find(booksId);
-//        bookEntity.setAuthors(list);
-//        LOGGER.log(Level.INFO, "Termina proceso de reemplazar los autores del libro con id = {0}", booksId);
-//        return bookPersistence.find(booksId).getAuthors();
-//    }
-//
-//    /**
-//     * Desasocia un Author existente de un Book existente
-//     *
-//     * @param booksId Identificador de la instancia de Book
-//     * @param authorsId Identificador de la instancia de Author
-//     */
-//    public void removeAuthor(Long booksId, Long authorsId) {
-//        LOGGER.log(Level.INFO, "Inicia proceso de borrar un autor del libro con id = {0}", booksId);
-//        AuthorEntity authorEntity = authorPersistence.find(authorsId);
-//        BookEntity bookEntity = bookPersistence.find(booksId);
-//        bookEntity.getAuthors().remove(authorEntity);
-//        LOGGER.log(Level.INFO, "Termina proceso de borrar un autor del libro con id = {0}", booksId);
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.dietas.ejb;
+
+import co.edu.uniandes.csw.dietas.entities.DietaEntity;
+import co.edu.uniandes.csw.dietas.entities.SemanaEntity;
+import co.edu.uniandes.csw.dietas.persistence.DietaPersistence;
+import co.edu.uniandes.csw.dietas.persistence.SemanaPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Alejandra Bravoa
+ */
+@Stateless
+public class DietaSemanasLogic {
+    private static final Logger LOGGER = Logger.getLogger(DietaSemanasLogic.class.getName());
+
+    @Inject
+    private DietaPersistence dietaPersistence;
+
+    @Inject
+    private SemanaPersistence semanaPersistence;
+
+    /**
+     * Asocia un Dieta existente a un Semana
+     *
+     * @param semanasId Identificador de la instancia de Semana
+     * @param dietaId Identificador de la instancia de Dieta
+     * @return Instancia de DietaEntity que fue asociada a Semana
+     */
+    public DietaEntity addDieta(Long semanasId, Long dietaId) {
+        DietaEntity dietaEntity = dietaPersistence.findByID(dietaId);
+        SemanaEntity semanaEntity = semanaPersistence.findId(semanasId);
+        semanaEntity.setDieta(dietaEntity);
+        return dietaPersistence.findByID(dietaId);
+    }
+
+    /**
+     * Obtiene una colección de instancias de DietaEntity asociadas a una
+     * instancia de Semana
+     *
+     * @param semanasId Identificador de la instancia de Semana
+     * @return Colección de instancias de DietaEntity asociadas a la instancia
+     * de Semana
+     */
+    public List<SemanaEntity> getSemanas(Long dietasId) {
+        return dietaPersistence.findByID(dietasId).getSemanas();
+    }
+
+    /**
+     * Obtiene una instancia de DietaEntity asociada a una instancia de Semana
+     *
+     * @param semanasId Identificador de la instancia de Semana
+     * @param authorsId Identificador de la instancia de Dieta
+     * @return La entidad del Autor asociada al libro
+     */
+    public SemanaEntity getSemana(Long semanasId, Long dietaId) {
+        List<SemanaEntity> semanas = dietaPersistence.findByID(semanasId).getSemanas();
+        SemanaEntity semanaEntity = semanaPersistence.findId(semanasId);
+        int index = semanas.indexOf(semanaEntity);
+        if (index >= 0) {
+            return semanas.get(index);
+        }
+        return null;
+    }
+
+    /**
+     * Remplaza las instancias de Dieta asociadas a una instancia de Semana
+     *
+     * @param semanasId Identificador de la instancia de Semana
+     * @param list Colección de instancias de DietaEntity a asociar a instancia
+     * de Semana
+     * @return Nueva colección de SemanaEntity asociada a la instancia de Semana
+     */
+    public List<SemanaEntity> replaceSemanas(Long dietasId, List<SemanaEntity> list) {
+        DietaEntity dietaEntity = dietaPersistence.findByID(dietasId);
+        dietaEntity.setSemanas(list);
+        return dietaPersistence.findByID(dietasId).getSemanas();
+    }
+
+    /**
+     * Desasocia un Dieta existente de un Semana existente
+     *
+     * @param semanaId Identificador de la instancia de Semana
+     * @param dietasId Identificador de la instancia de Dieta
+     */
+    public void removeDieta(Long dietasId, Long semanaId) {
+        SemanaEntity semanaEntity = semanaPersistence.findId(semanaId);
+        DietaEntity dietaEntity = dietaPersistence.findByID(dietasId);
+        dietaEntity.getSemanas().remove(semanaEntity);
+        semanaPersistence.delete(semanaId);
+    }
+}
